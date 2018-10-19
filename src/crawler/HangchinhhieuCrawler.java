638d0256cbe14f044814f971bac75fb297283849
Dang Xuan Anh <dangxuananh1997@gmail.com>
2018-10-20 00:28:54 +0700

    HangchinhhieuCrawler +getAllProductLink()

--- src/crawler/HangchinhhieuCrawler.java
+++ src/crawler/HangchinhhieuCrawler.java
@@ -2,12 +2,16 @@ package crawler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import utilities.XMLUtilities;
 
 /**
@@ -19,9 +23,9 @@ public class HangchinhhieuCrawler {
     public HangchinhhieuCrawler() {
     }
     
-    private String getPaginationDomString(String urlString) {
+    private String getPaginationDomString(String url) {
         try {
-            BufferedReader reader = XMLUtilities.getBufferedReaderFromURL(urlString);
+            BufferedReader reader = XMLUtilities.getBufferedReaderFromURL(url);
             String line;
             String document = "";
             boolean isStart = false;
@@ -47,10 +51,10 @@ public class HangchinhhieuCrawler {
         return "";
     }
     
-    public int getLastPageNumber(String urlString) {
+    public int getLastPageNumber(String url) {
         int pageNum = 1;
         try {
-            String domString = getPaginationDomString(urlString);
+            String domString = getPaginationDomString(url);
             if (!domString.isEmpty()) {
                 Document document = XMLUtilities.parseStringToDom(domString);
                 XPath xPath = XMLUtilities.getXPath();
@@ -63,6 +67,67 @@ public class HangchinhhieuCrawler {
             Logger.getLogger(HangchinhhieuCrawler.class.getName()).log(Level.SEVERE, null, ex);
         }
         return pageNum;
+    }
+    
+    private String getProductDomString(String url) {
+        try {
+            BufferedReader reader = XMLUtilities.getBufferedReaderFromURL(url);
+            String line;
+            String document = "";
+            boolean isStart = false;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("<section id=\"insCollectionPage\">")) {
+                    isStart = true;
+                }
+                // replace entity &-; with a-z
+                if (isStart && line.contains("&") && line.contains(";")) {
+                    line = line.replaceAll("&", "a").replaceAll(";", "z");
+                }
+                // add img closing tag
+                if (isStart && line.contains("<img")) {
+                    line += "</img>";
+                }
+                // remove data-price with '<' line
+                if (isStart && line.contains("data-price")) {
+                    line = "";
+                }
+                if (isStart && !line.isEmpty()) {
+                    document += line.trim();
+                }
+                if (isStart && line.contains("</section>")) {
+                    break;
+                }
+            }
+            return document;
+        } catch (IOException ex) {
+            Logger.getLogger(HangchinhhieuCrawler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+    
+    public List<String> getAllProductLink(String url) {
+        List<String> productLinkArray = new LinkedList<>();
+        try {
+            int lastPageNumber = getLastPageNumber(url);
+            for (int page = 1; page <= lastPageNumber; page++) {
+                String domString = getProductDomString(url + "?page=" + page);
+                if (!domString.isEmpty()) {
+                    Document document = XMLUtilities.parseStringToDom(domString);
+                    XPath xPath = XMLUtilities.getXPath();
+                    NodeList productList = (NodeList) xPath.evaluate("//*[@id=\"pd_collection\"]/ul/li", document, XPathConstants.NODESET);
+                    if (productList != null) {
+                        for (int p = 0; p < productList.getLength(); p++) {
+                            Node product = productList.item(p);
+                            Node productLink = (Node) xPath.evaluate(".//div[@class=\"image-product\"]/a", product, XPathConstants.NODE);
+                            productLinkArray.add(productLink.getAttributes().getNamedItem("href").getTextContent());
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(HangchinhhieuCrawler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return productLinkArray;
     }
     
 }
